7fb927e990c207bd923e38980e03581b17f62197
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-04-19 16:28:21 +0100

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,9 @@
 # installs flask from pip3
 
 package { 'flask':
-  ensure   => '2.1.0',
+  ensure   => installed,
+  name     => 'python3-flask',
   provider => 'pip3',
+  require  => Package['python3'],
+  ensure_version => '== 2.1.0',
 }
