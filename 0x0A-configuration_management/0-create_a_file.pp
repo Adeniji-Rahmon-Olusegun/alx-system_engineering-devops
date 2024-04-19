68d3b00bcdb26da6a3a28298eb1eddce06b80e50
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-04-19 16:07:45 +0100

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,8 @@
 # creates a file in /tmp
 
 file { 'school':
-  ensure  => present,
-  path    => '/tmp/school',
-  owner   => 'www-data',
-  group   => 'www-data'
-  mode    => '0744',
   content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
