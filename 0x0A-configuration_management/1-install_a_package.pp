d68778deb4e291405b9c0716b18cf362a082d476
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-04-23 16:29:12 +0100

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
 #!/usr/bin/pup
 # installs flask from pip using puppet
+
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
