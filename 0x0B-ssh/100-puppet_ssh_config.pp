ef754add44c32ad4892b2b56e26bc897c796caf4
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-04-23 16:00:43 +0100

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,13 @@
 # SSH client configuration
 
 file_line { 'Turn off passwd auth':
-  path  => '/etc/ssh/sshd_config',
-  line  => 'PasswordAuthentication no'
+  ensure => 'present',
+  path   => '/etc/ssh/sshd_config',
+  line   => 'PasswordAuthentication no'
 }
 
-file_line {
-  path  => '/etc/ssh/ssh_config',
-  line  => 'IdentityFile ~/.ssh/school',
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/school',
 }
