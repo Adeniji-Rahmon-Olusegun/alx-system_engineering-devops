6f273a222736f5c858917bd90b559b971df46b78
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-04-24 19:20:55 +0100

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,37 +1,24 @@
-# installs nginx on ubuntu using puppet
+# configues nginx on Ubuntu machine
+
+exec {'update system':
+  command => '/usr/bin/apt-get update',
+}
 
 package { 'nginx':
-  ensure => installed,
+  ensure  => installed,
+  require => Exec['update system'],
 }
 
-service { 'nginx':
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+exec { 'redirect_me':
+  command  => 'sed -i "24i\     rewrite ^/redirect_me https://www.youtube.com/watch?v=TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell',
+}
+
+service {'nginx':
   ensure  => running,
-  enable  => true,
   require => Package['nginx'],
-}
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => '
-server {
-    listen 80 default_server;
-    listen [::]: default_server;
-
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-
-    location / {
-        try_files $uri $uri/ =404;
-    }                                                             
-    location = /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-
-    location = / {
-        return 200 "Hello World!\n";
-    }
-}
-',
-  require => Package['nginx'],
-  notify  => Service['nginx'],
 }
