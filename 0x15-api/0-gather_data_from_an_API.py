fd30019a56106d9792c81868154ae12db5f8ae83
Adeniji-Rahmon-Olusegun <rsadeniji@gmail.com>
2024-05-20 22:10:12 +0100

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,27 +4,28 @@
 import requests
 import sys
 
+
+def fetch_info(rest_url):
+    """Gets information or data from the desired url"""
+    response = requests.get(rest_url)
+
+    response.raise_for_status()
+
+    return response.json()
+
 if __name__ == "__main__":
-
-    def fetch_info(rest_url):
-        """Gets information or data from the desired url"""
-        response = requests.get(rest_url)
-
-        response.raise_for_status()
-
-        return response.json()
 
     user_info = fetch_info(
             f"https://jsonplaceholder.typicode.com/users/{sys.argv[1]}"
     )
 
-    employee = user_info["name"]
+    employee = user_info.get("name")
 
     tasks = fetch_info(
             f"https://jsonplaceholder.typicode.com/todos?userId={sys.argv[1]}"
     )
 
-    finished_tasks = [done["title"] for done in tasks if done["completed"]]
+    finished_tasks = [done.get("title") for done in tasks if done.get("completed")]
 
     all_task = len(tasks)
 
